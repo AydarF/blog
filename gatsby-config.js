05d72ad00a17b12e18ccdaefcef740ffb83ddeb4
AydarF <aydar_fayzullin@yahoo.com>
2019-11-08 01:03:02 -0500

    Add GraphQL and Helmet to render site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,10 @@
 module.exports = {
+    siteMetadata: {
+        title: 'Aydar the Dev Blog',
+        description: 'The blog will document the journey to become a programmer'
+    },
     plugins: [
-        'gatsby-plugin-emotion'
+        'gatsby-plugin-emotion',
+        'gatsby-plugin-react-helmet'
     ]
 }
