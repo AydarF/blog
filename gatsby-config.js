d3a4102147b983904e0ec9b9201690d0c919f89e
AydarF <aydar_fayzullin@yahoo.com>
2019-11-11 20:05:53 -0500

    Create pages using mdx & create posts layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,25 @@
 module.exports = {
     siteMetadata: {
         title: 'Aydar the Dev Blog',
-        description: 'The blog will document the journey to become a programmer'
+        description: 'This blog will document my journey to become a programmer'
     },
     plugins: [
         'gatsby-plugin-emotion',
-        'gatsby-plugin-react-helmet'
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-plugin-mdx',
+            options: {
+                defaultLayouts: {
+                    default: require.resolve('./src/components/layout.js')
+                }
+            }
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'posts',
+                path: 'posts'
+            }
+        }
     ]
 }
