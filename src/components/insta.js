d9b9826c5fda9bf9ace5526ea242deb338e09b99
AydarF <aydar_fayzullin@yahoo.com>
2019-11-12 20:43:45 -0500

    Get the image data from Instagram

--- src/components/insta.js
+++ src/components/insta.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Image from 'gatsby-image';
 import { css } from '@emotion/core';
+import useInstagram from '../hooks/use-instagram';
 
 const Insta = () => {
-    // sfgsd
+    const instaPhotos = useInstagram();
+    const { username} = instaPhotos[0];
 
     return (
         <>
-            <h2>Insta posts</h2>
-            <div>show posts here</div>
-            <a href={`https://instagram.com/gatsbyjs`}>
+            <h2>Instagram posts from @{username}</h2>
+            <div>
+                {instaPhotos.map(photo => (
+                    <a key={photo.id} href={`https://instagram.com/p/${photo.id}`}></a>
+                ))}
+            </div>
+            <a href={`https://instagram.com/${username}`}>
                 Seee more posts on instagram
             </a>
         </>
