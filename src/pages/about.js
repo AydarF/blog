618719a766505c5fe62d0e57685abc252fd3eafc
AydarF <aydar_fayzullin@yahoo.com>
2019-12-02 21:09:15 -0500

    Update portfolio link & update about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { css } from '@emotion/core';
 import Layout from '../components/layout';
 
 export default () => (
     <Layout>
         <h1>About Me</h1>
-        <p>This is my personal blog</p>
+        <p css={css`
+            margin: 2rem auto;
+            line-height: 1.6;
+            `
+        }>
+            I'm a self-taught Front End Developer and I love building things with JavaScript!
+            That said, I'd love to learn and master Back End (yay to Python!) and, eventually, 
+            Machine Learning (because let's face it someone has to teach 
+            those robots to take over the world (c) <span role="img" aria-label="Smiley face">😃</span>)
+        </p>
         <Link to="/">&larr; Take me back Home!</Link>
     </Layout>
 )
