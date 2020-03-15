d57972aacfffeae9f7690b7f7f5138ae88eb03f9
AydarF <aydar_fayzullin@yahoo.com>
2020-03-15 00:30:25 -0400

    Add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,56 +1,66 @@
 module.exports = {
-    siteMetadata: {
-        title: 'Aydar the Dev Blog',
-        description: 'This blog will document my journey to become a programmer'
+  siteMetadata: {
+    title: 'Aydar the Dev Blog',
+    description: 'This blog will document my journey to become a programmer',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+          },
+        ],
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+          },
+        ],
+        extensions: [`.mdx`, `.md`],
+      },
     },
-    plugins: [
-        'gatsby-plugin-emotion',
-        'gatsby-plugin-react-helmet',
-        'gatsby-transformer-sharp',
-        'gatsby-plugin-sharp',
-        {
-            resolve: 'gatsby-plugin-mdx',
-            options: {
-                defaultLayouts: {
-                    default: require.resolve('./src/components/layout.js')
-                },
-                gatsbyRemarkPlugins: [{
-                    resolve: 'gatsby-remark-images'
-                }],
-                plugins: [{
-                    resolve: 'gatsby-remark-images'
-                }],
-                extensions: [`.mdx`, `.md`],
-            }
-        },
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'posts',
-                path: 'posts'
-            }
-        },
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'images',
-                path: 'images'
-            }
-        },
-        {
-            resolve: 'gatsby-source-instagram',
-            options: {
-                username: 'gatsbyjs'
-            }
-        },
-        {
-            resolve: 'gatsby-plugin-webpack-bundle-analyzer',
-            options: {
-                production: true,
-                disable: !process.env.ANALYZE_BUNDLE_SIZE,
-                generateStatsFile: true,
-                analyzerMode: 'static'
-            }
-        }
-    ]
-}
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: 'images',
+      },
+    },
+    {
+      resolve: 'gatsby-source-instagram',
+      options: {
+        username: 'gatsbyjs',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-100576236-2',
+      },
+    },
+  ],
+};
